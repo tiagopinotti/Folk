e28cd6411d8721b8c8a82bb9ba1d85902517a0b4
Eduardo Mendes <eduardowilliangm@gmail.com>
2018-02-16 23:28:53 -0200

    Main Screen Adjustments

--- app/src/main/java/folk/jettech/com/folk/MainScreenActivity.java
+++ app/src/main/java/folk/jettech/com/folk/MainScreenActivity.java
@@ -3,28 +3,44 @@ package folk.jettech.com.folk;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 
 import com.arlib.floatingsearchview.FloatingSearchView;
 import com.arlib.floatingsearchview.suggestions.model.SearchSuggestion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import folk.jettech.com.folk.res.recycler.adapater.RecyclerLineAdapter;
+import folk.jettech.com.folk.res.recycler.res.recycler.entities.Persona;
 
 public class MainScreenActivity extends AppCompatActivity {
 
     private FloatingSearchView mSearchInputView;
     private DrawerLayout mDrawerLayout;
-    private ListView mDrawerList;
-
-    private String[] items = {"Item","Item","Item","Item","Item","Item",};
+    private RecyclerView mRecyclerView;
+    private RecyclerLineAdapter recyclerLineAdapter;
+    private List<Persona> personas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerList = (ListView)findViewById(R.id.drawer_list);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view_main);
+        personas = new ArrayList<>();
 
-        mDrawerList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,items));
+        personas.add(new Persona("Item", "Item description"));
+        personas.add(new Persona("Item", "Item description"));
+        personas.add(new Persona("Item", "Item description"));
+        personas.add(new Persona("Item", "Item description"));
+        personas.add(new Persona("Item", "Item description"));
+        personas.add(new Persona("Item", "Item description"));
+
+        recyclerLineAdapter = new RecyclerLineAdapter(personas);
+        mRecyclerView.setAdapter(recyclerLineAdapter);
+        mRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
 
         mSearchInputView = (FloatingSearchView) findViewById(R.id.floating_search_view);
         mSearchInputView.setOnSearchListener(new FloatingSearchView.OnSearchListener() {
@@ -40,7 +56,6 @@ public class MainScreenActivity extends AppCompatActivity {
         });
 
         mSearchInputView.attachNavigationDrawerToMenuButton(mDrawerLayout);
-
 
     }
 }
